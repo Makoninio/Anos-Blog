cee84225166beef9031ec40140f50ef33e034831
Makoninio <115080189+Makoninio@users.noreply.github.com>
2025-07-20 12:13:59 -0400

    Trying to fix some caching issues when dispalying blogs

--- anos-blogs/app/book-blog/page.tsx
+++ anos-blogs/app/book-blog/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import React from "react";
 import { client, urlFor } from "../../lib/sanity";
 import { postsQuery, categoriesQuery, currentlyReadingQuery } from "../../lib/queries";
--- anos-blogs/app/page.tsx
+++ anos-blogs/app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = "force-dynamic";
 import Link from "next/link";
 import { client, urlFor } from "../lib/sanity";
 import { latestPostsQuery } from "../lib/queries";
